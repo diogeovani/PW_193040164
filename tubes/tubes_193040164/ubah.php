c73932461189807e7051f01e0d9b30ad5e2138ed
Diogeovani <59018305+diogeovani@users.noreply.github.com>
2020-05-18 08:00:30 +0700

    update

--- tubes/tubes_193040164/ubah.php
+++ tubes/tubes_193040164/ubah.php
@@ -35,53 +35,47 @@ if (isset($_POST['ubah'])) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Ubah Data Musik</title>
+  <title>Tambah Data</title>
+  <!-- Compiled and minified CSS -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 
 <body>
-  <h3>Form Ubah Data Musik</h3>
-  <form action="" method="POST" enctype="multipart/form-data">
-    <input type="hidden" name="id" value="<?= $m['id']; ?>">
-    <ul>
-      <li>
-        <label>
-          Nama alat :
-          <input type="text" name="nama" autofocus required value="<?= $m['nama']; ?>">
-        </label>
-      </li>
-      <li>
-        <label>
-          Merek :
-          <input type="text" name="merek" required value="<?= $m['merek']; ?>">
-        </label>
-      </li>
-      <li>
-        <label>
-          Alamat :
-          <input type="text" name="alamat" required value="<?= $m['alamat']; ?>">
-        </label>
-      </li>
-      <li>
-        <label>
-          Harga :
-          <input type="text" name="harga" required value="<?= $m['harga']; ?>">
-        </label>
-      </li>
-      <li>
-        <input type="hidden" name="gambar_lama" value="<?= $m['gambar']; ?>">
-        <label>
-          Gambar :
-          <input type="file" name="gambar" class="gambar" onchange="previewImage()">
-        </label>
-        <img src="img/<?= $m['gambar']; ?>" width="120" style="display: block;" class="img-preview">
-      </li>
-      <li>
-        <button type="submit" name="ubah">Ubah Data!</button>
-      </li>
-    </ul>
+  <h3 class="center">Form Ubah Data </h3>
+  <form align="center" action="" method="POST">
+    <div class="row">
+      <div class="col s12">
+        <div class="row">
+          <div class="input-field col s12">
+            <i class="material-icons prefix">dehaze</i>
+            <input type="file" name="gambar" class="gambar" onchange="previewImage()" id="gambar autocomplete-input" class="autocomplete">
+            <label for="cover"></label>
+            <img src="img/<?= $m['gambar']; ?>" width="120" style="display: block;" class="img-preview">
+          </div>
+          <div class="input-field col s12">
+            <i class="material-icons prefix">dehaze</i>
+            <input type="text" name="alat_musik" autofocus required value="<?= $m['alat_musik']; ?>">
+            <label for="judul buku">Alat musik</label>
+          </div>
+          <div class="input-field col s12">
+            <i class="material-icons prefix">dehaze</i>
+            <input type="text" name="alamat" id="alamat" required value="<?= $m['alamat']; ?>">
+            <label for="penulis">Alamat</label>
+          </div>
+          <div class="input-field col s12">
+            <i class="material-icons prefix">dehaze</i>
+            <input type="text" name="harga" id="harga" required value="<?= $m['harga']; ?>">
+            <label for="harga">Harga </label>
+          </div>
+        </div>
+      </div>
+    </div>
+    <button type="submit" name="tambah" style="text-decoration: none; color: black;" class="btn waves-effect waves-light blue">Ubah Data</button>
+    <a href="index.php" style="text-decoration: none; color: black;" class="btn waves-effect waves-light blue">Kembali</a>
   </form>
-
-  <script src="js/script.js"></script>
+  <!-- Compiled and minified JavaScript -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 </body>
 
 </html>
